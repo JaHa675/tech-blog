b85135725b2ad031aec1deac2499355d6d8bc213
James Harding <99635678+JaHa675@users.noreply.github.com>
2022-06-10 17:03:07 -0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # tech-blog
+
+![License: MIT](https://img.shields.io/badge/license-MIT-blue)
+
+  - [Tech blog](#tech-blog)
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution guidelines](#contribution-guidelines)
+  - [Questions](#questions)
+  - [License](#license)
+
+## Description
+
+A website to demonstrate my capability in using handlebars. While not amazingly styled, It does still display posts and their comments as they show up and let you delete them if you want to.  
+-[Here's a link](https://james-harding-portfolio.herokuapp.com/)
+
+---
+
+## Installation
+
+If you want to work on this locally you will need to run npm i and also set up a . env file. It also requires that you set up the database using mysql.
+
+---
+
+## Usage
+
+Create an account then post some blogs! then feel  free to comment on things other people have made
+
+---
+
+## Contribution guidelines
+
+Create an issue here on github to get my attention about this repo. Alternatively you can use the questions section at the bottom of the page to message me.
+
+---
+
+## Questions
+
+If you have any questions, reach me on [github](https://github.com/JaHa675)
+
+or email me here at James.Harding675@gmail.com
+
+---
+
+## License
+
+MIT
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,27 +1,28 @@
 console.log('comments linked')
+const inputBox = document.querySelector('.comment-boxes')
 const commentButtons = document.querySelectorAll('.comment-button')
 commentButtons.forEach((ele) => {
     ele.addEventListener('click', e => {
         console.log('doggie')
         e.preventDefault()
-        const blogComment = ele.querySelector("input").value
+        const blogComment = inputBox.value
         let id = e.target.getAttribute("id");
         console.log(blogComment + id)
-        // fetch(`/api/comments`, {
-        //     method: "POST",
-        //     body: JSON.stringify({
-        //         id,
-        //         blogComment
-        //     }),
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     }
-        // }).then(res => {
-        //     if (res.ok) {
-        //         console.log('res was ok')
-        //     } else {
-        //         alert("Comment could not be commented")
-        //     }
-        // })
+        fetch(`/api/comments`, {
+            method: "POST",
+            body: JSON.stringify({
+                id,
+                blogComment
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => {
+            if (res.ok) {
+                console.log('res was ok')
+            } else {
+                alert("Comment could not be commented")
+            }
+        })
     })
 })
